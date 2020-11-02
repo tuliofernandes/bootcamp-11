98dbeca58e652a7141d27b2450780ea305a701f6
Tulio Fernandes <tulio.fernandes@itst.com.br>
2020-11-02 16:00:17 -0300

    Configurando EditorConfig, ESLint e Prettier

---
+++ primeiro-projeto-node/src/server.ts
@@ -0,0 +1,12 @@
+import express, { response } from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello world' })
+})
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333')
+})
