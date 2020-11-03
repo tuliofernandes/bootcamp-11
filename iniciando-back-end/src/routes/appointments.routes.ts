a189d26674892254d2eea280d45e222fbb892913
Tulio Fernandes <tulio.fernandes@itst.com.br>
2020-11-03 17:22:06 -0300

    Instalando e configurando TypeORM

---
+++ iniciando-back-end/src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+    try {
+        const { provider, date } = request.body;
+
+        const parsedDate = parseISO(date);
+
+        const createdAppointment = new CreateAppointmentService(
+            appointmentsRepository,
+        );
+
+        const appointment = createdAppointment.execute({
+            date: parsedDate,
+            provider,
+        });
+
+        return response.json(appointment);
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
+    }
+});
+
+export default appointmentsRouter;
