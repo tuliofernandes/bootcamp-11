b18a80dc8057945f0c6a184415974525c36eb446
Tulio Fernandes <tulio.fernandes@itst.com.br>
2020-11-03 19:27:04 -0300

    Reestruturando para arquivoes para TypeORM

--- iniciando-back-end/src/services/CreateAppointmentService.ts
+++ iniciando-back-end/src/services/CreateAppointmentService.ts
@@ -1,38 +1,37 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-    provider: string;
-    date: Date;
+  provider: string;
+  date: Date;
 }
 
 class CreateAppointmentService {
-    private appointmentsRepository: AppointmentsRepository;
+  public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
+    const appointmentDate = startOfHour(date);
+
+    const appointmentInSameDateFound = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (appointmentInSameDateFound) {
+      throw Error('This appointment is already booked');
     }
 
-    public execute({ provider, date }: RequestDTO): Appointment {
-        const appointmentDate = startOfHour(date);
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
 
-        const appointInSameDateFound = this.appointmentsRepository.findByDate(
-            appointmentDate,
-        );
+    await appointmentsRepository.save(appointment);
 
-        if (appointInSameDateFound) {
-            throw Error('This appointment is already booked');
-        }
-
-        const appointment = this.appointmentsRepository.create({
-            provider,
-            date: appointmentDate,
-        });
-
-        return appointment;
-    }
+    return appointment;
+  }
 }
 
 export default CreateAppointmentService;
