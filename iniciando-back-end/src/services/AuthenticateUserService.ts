504ce70f4c5cc0e6b28fd37b23bdfe695bd3677d
Tulio Fernandes <tulio.fernandes@itst.com.br>
2020-11-10 19:54:22 -0300

    Validando credenciais

---
+++ iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface ResponseDTO {
+  user: User;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: RequestDTO): Promise<ResponseDTO> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email or password');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email or password');
+    }
+
+    return { user };
+  }
+}
+
+export default AuthenticateUserService;
