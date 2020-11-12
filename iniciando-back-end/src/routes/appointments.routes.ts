e455b96bc85298e8f30ade211afa0bdd5b858b24
Tulio Fernandes <tulio.fernandes@itst.com.br>
2020-11-11 22:20:08 -0300

    Tratando erros conhecidos e desconhecidos

--- iniciando-back-end/src/routes/appointments.routes.ts
+++ iniciando-back-end/src/routes/appointments.routes.ts
@@ -20,22 +20,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createdAppointment = new CreateAppointmentService();
+  const createdAppointment = new CreateAppointmentService();
 
-    const appointment = await createdAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await createdAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (error) {
-    return response.status(error.statusCode).json({ error: error.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- iniciando-back-end/src/routes/user.routes.ts
+++ iniciando-back-end/src/routes/user.routes.ts
@@ -11,29 +11,25 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json(userWithoutPassword);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json(userWithoutPassword);
 });
 
 usersRouter.patch(
